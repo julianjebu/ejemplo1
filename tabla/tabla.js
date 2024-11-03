877c908437fc9a37a4c648d0857a8ceba6e64332
julianjebu <juanjebu@gmail.com>
2024-11-03 12:06:41 -0500

    pase los archivos a una carpeta para organizarlos

---
+++ tabla/tabla.js
@@ -0,0 +1,32 @@
+/* ciclo for imagina que tines que esribir "hola" s veces  en lugar de escribir 5 veces para que la computadora lo haga por ti un ciclo for es ultil cuando sabes exactamente la cantidad de veces que vas a repetir codigo
+
+*/
+/* const titulo = document.getElementById("titulo");
+const tablaResultado = document.getElementById("tablaResultado");
+let tabla = parseInt(prompt("Inngresa numero"));
+titulo.innerHTML = "Tabla del " + tabla + " con for " + "<br>";
+
+let resultado1 = "";
+for (let index = 0; index <= 10; index++) {
+  resultado1 += tabla + " x " + index + " = " + tabla * index + "<br>";
+}
+
+tablaResultado.innerHTML += resultado1 + `<h2> tabla del ${tabla} con while</h3>`;
+
+let contador = 0;
+let resultado2 = "";
+while (contador <= 10) {
+  resultado2 += tabla + " x " + contador + " = " + tabla * contador + "<br>";
+  contador++;
+}
+
+tablaResultado.innerHTML += resultado2 + `<h2> tabla del ${tabla} con do while</h3>`;
+
+let contador2 = 0;
+let resultado3 = "";
+do {
+  resultado3 += tabla + " x " + contador2 + " = " + tabla * contador2 + "<br>";
+  contador2++;
+} while (contador2 < 10);
+
+tablaResultado.innerHTML += resultado3 + "<br>"; */
